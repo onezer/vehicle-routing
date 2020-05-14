7ef903a1e21cd5eaf943e25999ea4ed4eb86c58e
Viktor Pasti <pastivv@gmail.com>
2020-05-14 22:10:39 +0200

    crossover

--- src/entity.cpp
+++ src/entity.cpp
@@ -24,7 +24,54 @@ float Entity::Cost() const
 Gene Entity::Crossover(const Gene & other) const
 {
 	Gene newGene;
+	newGene.groups = gene.groups;
+	if (other.groups.size() != gene.groups.size()) throw std::exception("Illegal crossover!");
 
+	for (int i = 0; i < gene.groups.size(); ++i) {
+		char label = gene.groups[i];
+
+		int order_size = gene.order.at(label).size();
+
+		bool* choose = new bool[order_size];
+
+		//inicialize choose bool array with random values
+		for (int j = 0; j < order_size; ++j) {
+			choose[j] = rand() % 100 < 50;
+		}
+
+		std::vector<int> temp;
+		std::vector<int> order;
+
+		//mixing the two order vectors
+		for (int j = 0; j < order_size; ++j) {
+			temp.push_back(gene.order.at(label)[j]);
+			temp.push_back(other.order.at(label)[j]);
+		}
+
+		int k = 0;
+		std::unordered_map<int, bool> visited;
+
+		//removing duplicates
+		for(int j = 0; j < temp.size(); ++j){
+			if (visited.find(temp[j]) == visited.end()) {
+				if (choose[k]) {
+					order.push_back(temp[j]);
+					++k;
+				}
+
+				visited.insert({temp[j], true});
+			}
+			else {
+				if (choose[k] == false) {
+					order.push_back(temp[j]);
+					++k;
+				}
+			}
+
+		}
+
+		newGene.order.insert({ label, order });
+	}
 
 	return newGene;
 }
